5eb234ccc67536f1b4cb9014bb1888e6fc0fab7d
1w6ts <xype.discord@gmail.com>
2025-04-19 22:57:24 +0200

    feat: certificate generation with multiple auth methods

--- cerbot/setup-certbot.sh
+++ cerbot/setup-certbot.sh
@@ -24,3 +24,68 @@ if [[ "$1" == "--help" || $# -eq 0 ]]; then
 fi
 
 echo  "Starting Certbot installation and certificate setup..."
+
+function install_certbot {
+    echo "Installing Cerbot..."
+
+    # Detect OS
+    if [ -f /etc/debian_version ]; then
+    # Debian/Ubunut
+        apt-get update
+        apt-get install -y certbot python3-certbot-nginx python3-certbot-apache
+    elif [ -f /etc/redhat-release ]; then
+        if grep -q "release 7" /etc/readhat-release; then
+            yum install -y epel-release
+            yum install -y certbot certbot-nginx certbot-apache
+        else
+            dnf install -y epel-release
+            dnf install -y certbot python3-certbot-nginx python3-certbot-apache
+        fi
+    else
+        echo "Unsupported OS. Please install Certbot manually."
+        exit 1
+    fi
+
+    # Verify Installation
+    if command -v certbot &> /dev/null; then
+        echo "Certbot installed successfully."
+    else
+        echo "Certbot installation failed."
+        exit 1
+    fi
+}
+
+function generate_certificate {
+    echo "Generating SSL certificates..."
+
+    local cmd="certbot certonly"
+
+
+    if [ -n "$EMAIL" ]; then
+        cmd="$cmd --email $EMAIL"
+    else
+        cmd="$cmd --register-unsafely-without-email"
+    fi
+
+    # add domains
+    for domain in "${DOMAINS[@]}"; do
+        cmd="$cmd -d $domain"
+    done
+
+    if [ "$WEB_SERVER" == "nginx" ]; then
+        cmd="$cmd --nginx"
+    elif [ "$WEB_SERVER" == "apache"]; then
+        cmd="$cmd --apache"
+    fi
+
+    if [ "$STAGING" -eq 1 ]; then
+        cmd="$cmd --staging"
+    fi
+
+    cmd="$cmd --non-interactive --agree-tos"
+
+    echo "Executing: $cmd"
+    eval "$cmd"
+
+    echo "Certificate generation complete."
+}
